3c31843ba0f48c38e7609e7eb16c4ea8665f0efa
Alix Boc <alix.boc@code3.ca>
2022-09-24 14:42:12 -0400

    argument de l aligne de commande

--- addirap.py
+++ addirap.py
@@ -1,24 +1,49 @@
 """
 =============================================================================
-= addirap : générateur d'addition pour l'approche addirap 
+= addirap : générateur d'addition pour l'approche addirap
 = auteur : Alix boc
 = date : 2022
 =============================================================================
 """
 from random import randint
+import sys
 
-nbOperations = 50
-noOperation = 0
-maxValue = 9
-minValue = 1
-operations = {}
+def generate(nbOperations=50,minValue=1,maxValue=9):
+    operations = {}
+    noOperation = 0
+    while noOperation < nbOperations:
+        a = randint(minValue,maxValue)
+        b = randint(minValue,maxValue)
+        if (a,b) not in operations.keys():
+            operations[(a,b)] = 1
+            noOperation += 1
+    return operations
 
-while noOperation < nbOperations:
-    a = randint(minValue,maxValue)
-    b = randint(minValue,maxValue)
-    if (a,b) not in operations.keys():
-        operations[(a,b)] = a+b
-        noOperation += 1
+def afficher(operations,operation="+",output=sys.stdout):
+    for key in operations.keys():
+        print(key[0], operation, key[1], "=", file=output)
 
-for key in operations.keys():
-    print(key[0], "+", key[1], "=")
+def main(parametres):
+    nbOperations=50
+    operation = "+"
+    minValue = 1
+    maxValue = 9
+    if len(parametres) > 1:
+        for param in parametres:
+            print(param)
+            tab = param.split("=")
+            print(tab)
+            if tab[0] == "operation":
+                operation = tab[1]
+            elif tab[0] == "nbOperations":
+                nbOperations = int(tab[1])
+            elif tab[0] == "minValue":
+                minValue = int(tab[1])
+            elif tab[0] == "maxValue":
+                maxValue = int(tab[1])
+            else:
+                print("parametre inconnu :",tab[0])
+    afficher(generate(nbOperations=nbOperations,minValue=minValue,maxValue=maxValue),operation=operation)
+
+if __name__ == "__main__":
+    main(sys.argv)
